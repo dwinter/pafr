270a992b998fd97a64388f931a556863d685fcd4
dwinter <david.winter@gmail.com>
2019-09-03 11:23:57 +1200

    caught bug in filtering 2nd alignments

--- R/filter_ali.r
+++ R/filter_ali.r
@@ -2,7 +2,7 @@
 filter_secondary_alignments <- function(ali, remove_inversions=FALSE){
     if( "tp" %in% names(ali) ){
         if(remove_inversions){
-            return(ali[ ali$tp ==  "P"])
+            return(ali[ ali$tp ==  "P",])
         } 
         return(ali[ ali$tp %in% c("I", "i", "P"),])
     }   
