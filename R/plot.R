92b55c6ee5c4d25c81d6e2da474852e9ace7448a
dwinter <david.winter@gmail.com>
2019-09-06 11:35:58 +1200

    import top_n for sorting chroms by qstart

--- R/plot.R
+++ R/plot.R
@@ -115,7 +115,7 @@ plot_coverage <- function(ali, target=TRUE, fill_colour="forestgreen"){
     
 }
     
-
+#' @importFrom dplyr top_n
 #' @importFrom utils head
 order_seqs <- function(ali, by = c("size", "qstart", "provided", "asis"), ordering = list() ){    
     by <- match.arg(by)
