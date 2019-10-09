34b0f1b7f5b25e5a2ae186624af719c72d511299
dwinter <david.winter@gmail.com>
2019-10-10 11:24:33 +1300

    Start on #18, tidy up dotplot code

--- R/dotplot.R
+++ R/dotplot.R
@@ -10,11 +10,10 @@
 #' data.frame with one column of sequence names and another column containing
 #' the length of each sequence
 #' @export
-chrom_sizes <- function(ali){
-    res <- list(qlens = unique(ali[,c("qname", "qlen")]),
-         tlens = unique(ali[,c("tname", "tlen")])
-    )
-    if('pafr' %in% class(ali)){
+chrom_sizes <- function(ali) {
+    res <- list(qlens = unique(ali[, c("qname", "qlen")]),
+                tlens = unique(ali[, c("tname", "tlen")]))
+    if ("pafr" %in% class(ali)) {
         return(lapply(res, as.data.frame))
     }
     res
@@ -25,48 +24,54 @@ chrom_sizes <- function(ali){
 #' @importFrom dplyr top_n
 #' @importFrom dplyr group_by
 #' @importFrom utils head
-order_seqs <- function(ali, by, ordering = list() ){      
+order_seqs <- function(ali, by, ordering = list()) {
     chrom_lens <- chrom_sizes(ali)
-    qsum = sum(chrom_lens[["qlens"]][,2])
-    tsum = sum(chrom_lens[["tlens"]][,2])
-    if(by == "size"){
-        q_idx <- order(chrom_lens[["qlens"]][,2], decreasing = TRUE )
-        t_idx <- order(chrom_lens[["tlens"]][,2], decreasing = TRUE )
-        qmap  = structure(.Names=chrom_lens[["qlens"]][q_idx,1], c(0, head(cumsum(chrom_lens[["qlens"]][q_idx,2]),-1)))
-        tmap  = structure(.Names=chrom_lens[["tlens"]][t_idx,1], c(0, head(cumsum(chrom_lens[["tlens"]][t_idx,2]),-1)))
+    qsum <- sum(chrom_lens[["qlens"]][, 2])
+    tsum <- sum(chrom_lens[["tlens"]][, 2])
+    if (by == "size") {
+        q_idx <- order(chrom_lens[["qlens"]][, 2], decreasing = TRUE)
+        t_idx <- order(chrom_lens[["tlens"]][, 2], decreasing = TRUE)
+        qmap  <- structure(.Names = chrom_lens[["qlens"]][q_idx, 1],
+                        c(0, head(cumsum(chrom_lens[["qlens"]][q_idx, 2]), -1)))
+        tmap  <- structure(.Names = chrom_lens[["tlens"]][t_idx, 1],
+                        c(0, head(cumsum(chrom_lens[["tlens"]][t_idx, 2]), -1)))
     } else if (by == "qstart") {
         #TODO
         #qidx/map id duplicated from above. DRY out depending on how we
         #implement other options
-        q_idx <- order(chrom_lens[["qlens"]][,2], decreasing = TRUE )
-        qmap  = structure(.Names=chrom_lens[["qlens"]][q_idx,1], c(0, head(cumsum(chrom_lens[["qlens"]][q_idx,2]),-1)))
+        q_idx <- order(chrom_lens[["qlens"]][, 2], decreasing = TRUE)
+        qmap  <- structure(.Names = chrom_lens[["qlens"]][q_idx, 1],
+                        c(0, head(cumsum(chrom_lens[["qlens"]][q_idx, 2]), -1)))
         longest_by_target <- top_n(group_by(ali, tname), 1, alen)
-        t_idx  <- order(qmap[longest_by_target$qname] + longest_by_target$qstart)
-        tmap <- sort(structure(.Names = longest_by_target$tname[t_idx], c(0, head(cumsum(longest_by_target$tlen[t_idx]), -1))))        
+        t_idx <- order(qmap[longest_by_target$qname] + longest_by_target$qstart)
+        tmap <- sort(structure(.Names = longest_by_target$tname[t_idx],
+                         c(0, head(cumsum(longest_by_target$tlen[t_idx]), -1))))        
     } else if (by == "provided") {
-        if(length(ordering) != 2) {
+        if (length(ordering) != 2) {
             stop("To use 'provided' sequence ordering argument 'ordering' must by a list with two character vectors")             
         }
         qord <- ordering[[1]]
         tord <- ordering[[2]]
         q_idx <- match(qord, chrom_lens[["qlens"]][["qname"]])
-        if(any(is.na(q_idx))){
+        if (any(is.na(q_idx))) {
             msg <- paste("Sequence(s) provided for ordering are not present in alignment:\n",qord[is.na(q_idx)], "\n")
             stop(msg)
         }
         t_idx <- match(tord, chrom_lens[["tlens"]][["tname"]])
-        if(any(is.na(t_idx))){
+        if (any(is.na(t_idx))) {
             msg <- paste("Sequence(s) provided for ordering are not present in alignment:\n",tord[is.na(t_idx)], "\n")
             stop(msg)
         }
-        qmap  = structure(.Names=chrom_lens[["qlens"]][q_idx,1], c(0, head(cumsum(chrom_lens[["qlens"]][q_idx,2]),-1)))
-        tmap  = structure(.Names=chrom_lens[["tlens"]][t_idx,1], c(0, head(cumsum(chrom_lens[["tlens"]][t_idx,2]),-1)))
+        qmap <- structure(.Names = chrom_lens[["qlens"]][q_idx, 1],
+                        c(0, head(cumsum(chrom_lens[["qlens"]][q_idx, 2]), -1)))
+        tmap <- structure(.Names = chrom_lens[["tlens"]][t_idx, 1],
+                        c(0, head(cumsum(chrom_lens[["tlens"]][t_idx, 2]), -1)))
     }
     list(qmap = qmap, qsum = qsum, tmap = tmap, tsum = tsum)
 }
                
 
-add_pos_in_concatentaed_genome <- function(ali, maps){
+add_pos_in_concatentaed_genome <- function(ali, maps) {
     ali$concat_qstart <- ali$qstart + maps[["qmap"]][ali$qname]
     ali$concat_qend <- ali$qend + maps[["qmap"]][ali$qname]
     ali$concat_tstart <- ali$tstart + maps[["tmap"]][ali$tname]
@@ -75,33 +80,32 @@ add_pos_in_concatentaed_genome <- function(ali, maps){
 }
 
 
-dotplot_name_df <- function(seq_map, genome_len){
+dotplot_name_df <- function(seq_map, genome_len) {
     data.frame(seq_name = names(seq_map),
-               centre=seq_map + diff(c(seq_map, genome_len)/2)
-    )
+               centre = seq_map + diff(c(seq_map, genome_len) / 2))
 }
 
-check_ordering <- function(ali, ordering){
+check_ordering <- function(ali, ordering) {
     q_in_order <- unique(ali[["qname"]]) %in% ordering[[1]]
     t_in_order <- unique(ali[["tname"]]) %in% ordering[[2]]
-    if(any(!q_in_order)){
+    if (any(!q_in_order)) {
         msg <- paste("Dropping data from sequences absent from ordering:\n",
-                     paste(unique(ali[["qname"]])[!q_in_order], collapse=","))
-        warning(msg, call.=FALSE)
+                     paste(unique(ali[["qname"]])[!q_in_order], collapse = ","))
+        warning(msg, call. = FALSE)
     }
-    if(any(!t_in_order)){
+    if (any(!t_in_order)) {
         msg <- paste("Dropping data from sequences absent from ordering:\n",
-                     paste(unique(ali[["tname"]])[!t_in_order], collapse=","))
-        warning(msg, call.=FALSE)
+                     paste(unique(ali[["tname"]])[!t_in_order], collapse = ","))
+        warning(msg, call. = FALSE)
     }
-    return(invisible()) 
+    return(invisible())
 }
 
 #' Generate a dotplot from a paf alignment.
 #'
 #' @param ali pafr or tibble containing the genome alignment (as returned by
 #' \code{\link{read_paf}})
-#' @param order_by How should the query and target sequences be ordered in the 
+#' @param order_by How should the query and target sequences be ordered in the
 #' dotplot. One of 'size' (smallest-to-largest), 'qstart' (query organised
 #' smallest to largest, target by first match in the query genome or 'provided'
 #' (ordering specified in \code{ordering} argument
@@ -118,70 +122,79 @@ check_ordering <- function(ali, ordering){
 #' @param xlab character The x-axis label (defaults to "query").
 #' @param ylab character The y-axis label (defaults to "target").
 #' @param line_size the width of the line used to represent an alignment in the
-#' dotplot (defaults to 2).
-#' 
-#' @import ggplot2 
+#' dotplot (defaults to 2)
+#' @import ggplot2
 #' @export
-dotplot <- function(ali, order_by = c("size", "qstart", "provided"), label_seqs = FALSE, dashes=TRUE, ordering = list(), alignment_colour="black", xlab = "query", ylab="target", line_size=2){
+dotplot <- function(ali, order_by = c("size", "qstart", "provided"),
+                    label_seqs = FALSE, dashes = TRUE, ordering = list(),
+                    alignment_colour = "black", xlab = "query", ylab = "target",
+                    line_size=2) {
     by <- match.arg(order_by)
-    if(by == "provided"){
+    if (by == "provided") {
         check_ordering(ali, ordering)
         ali <- subset(ali, qname %in% ordering[[1]] & tname %in% ordering[[2]])
-    #    new_chrom_lens <- chrom_sizes(ali)
-    #    seq_maps[["qsum"]] <- new_chrom_lens[["qlens"]][,2]
-    #    seq_maps[["tsum"]] <- new_chrom_lens[["tlens"]][,2]
     }
     seq_maps <- order_seqs(ali, by, ordering)
     ali <- add_pos_in_concatentaed_genome(ali, seq_maps)
-    
-
-  p <- ggplot() + 
-      geom_segment(data=subset(ali, strand=="+"), aes(x=concat_qstart, xend=concat_qend, y=concat_tstart, yend=concat_tend), size=line_size, colour=alignment_colour) + 
-      geom_segment(data=subset(ali, strand=="-"), aes(x=concat_qend, xend=concat_qstart, y=concat_tstart, yend=concat_tend), size=line_size, colour=alignment_colour) + 
+    #now build the plot, first the aligments, easiest to do this +ve strand
+    #first, then -ve
+    p <- ggplot() +
+      geom_segment(data = subset(ali, strand=="+"), 
+         aes(x = concat_qstart, xend = concat_qend, y = concat_tstart, yend = concat_tend),
+         size=line_size, colour=alignment_colour) +
+      geom_segment(data = subset(ali, strand=="-"),
+        aes(x = concat_qend, xend = concat_qstart, y = concat_tstart, yend = concat_tend),
+        size = line_size, colour = alignment_colour) +
       coord_equal() +
-      scale_x_continuous(xlab, labels=Mb_lab) + 
-      scale_y_continuous(ylab, labels=Mb_lab)
-  if(dashes){
-      p <- p +  geom_hline(yintercept=c(seq_maps[["tmap"]], sum(unique(ali$tlen))), linetype=3) +
-                geom_vline(xintercept=c(seq_maps[["qmap"]], sum(unique(ali$qlen))), linetype=3)
+      scale_x_continuous(xlab, labels = Mb_lab) + 
+      scale_y_continuous(ylab, labels = Mb_lab)
+  if (dashes) {
+      p <- p + geom_hline(yintercept = c(seq_maps[["tmap"]], sum(unique(ali$tlen))), linetype = 3) +
+               geom_vline(xintercept = c(seq_maps[["qmap"]], sum(unique(ali$qlen))), linetype = 3)
   }
-  if (label_seqs){
+  if (label_seqs) {
     qname_df <- dotplot_name_df(seq_maps[["qmap"]], seq_maps[["qsum"]])
     tname_df <- dotplot_name_df(seq_maps[["tmap"]], seq_maps[["tsum"]])
-    p <- p + geom_text(data=qname_df, aes(label=seq_name, x=centre, y=0), vjust=1, check_overlap=TRUE) + 
-             geom_text(data=tname_df, aes(label=seq_name, x=0, y=centre), angle=90, vjust=0, check_overlap=TRUE) 
+    p <- p + geom_text(data = qname_df, aes(label = seq_name, x = centre, y = 0),
+                       vjust = 1, check_overlap = TRUE) +
+             geom_text(data = tname_df,
+                       aes(label = seq_name, x = 0, y = centre),
+                       angle = 90, vjust = 0, check_overlap = TRUE)
   }
   p
-
 }
 
 highlight_dotplot <- function(hl_source, ali, bed, ordered_by, ordering, fill, colour, alpha){
     map_n <-  if (hl_source == "query") 1 else 3
     seq_maps <- order_seqs(ali, ordered_by)
-    os = seq_maps[[map_n]][as.character(bed[["chrom"]])]
-    to_plot <- data.frame( i_start = bed$start + os, i_end = bed$end + os)
-    if(hl_source == "query"){
-        ystart = 0
+    os <- seq_maps[[map_n]][as.character(bed[["chrom"]])]
+    to_plot <- data.frame(i_start = bed$start + os, i_end = bed$end + os)
+    if (hl_source == "query") {
+        ystart <- 0
         yend <- seq_maps[["tsum"]]
-        xstart = "i_start"
-        xend = "i_end"        
+        xstart <- "i_start"
+        xend <- "i_end"
     } else {
-        ystart = "i_start"
-        yend = "i_end"        
+        ystart <- "i_start"
+        yend <- "i_end"        
         xend <- seq_maps[["qsum"]]
-        xstart = 0
+        xstart <- 0
     }
 
-    geom_rect(data=to_plot,
-              aes_string(xmin=xstart, xmax=xend, ymin=ystart, ymax= yend),
-               fill=fill, colour=colour, alpha=alpha)
+    geom_rect(data = to_plot,
+             aes_string(xmin = xstart, xmax = xend, ymin = ystart, ymax = yend),
+             fill = fill, colour = colour, alpha = alpha)
 }
 
 #' @rdname highlight_dotplot
 #' @export 
-highlight_query <- function(ali, bed, ordered_by = c("size", "qstart", "provided"), ordering=list(), fill="yellow", colour="black", alpha=0.6){
-    highlight_dotplot("query", ali, bed,  match.arg(ordered_by), ordering, fill, colour, alpha)
-}          
+highlight_query <- function(ali, bed, 
+                           ordered_by = c("size", "qstart", "provided"), 
+                           ordering = list(), fill = "yellow", colour = "black",
+                           alpha=0.6) {
+    highlight_dotplot("query", ali, bed,  
+                      match.arg(ordered_by), ordering, fill, colour, alpha)
+}
   
 #' Highlight segments of a query or target genome in a dotplot
 #'
@@ -208,5 +221,6 @@ highlight_query <- function(ali, bed, ordered_by = c("size", "qstart", "provided
 #' @rdname highlight_dotplot
 #' @export
 highlight_target <- function(ali, bed, ordered_by = c("size", "qstart", "provided"), ordering=list(), fill="yellow", colour="black", alpha=0.6){
-    highlight_dotplot("target", ali, bed,  match.arg(ordered_by), ordering, fill, colour, alpha)
+    highlight_dotplot("target", ali, bed,  
+                      match.arg(ordered_by), ordering, fill, colour, alpha)
 }
