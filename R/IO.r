aa7e31415996c336bbaccd1198dd9109e6591125
mpcox <murray.p.cox@gmail.com>
2019-10-26 21:46:07 +1300

    Updated documentation for IO

--- R/IO.r
+++ R/IO.r
@@ -1,22 +1,22 @@
-#' Read genomic intervals in bed format.
+#' Read genomic intervals in bed format
 #'
 #' The first three columns of the file specified by \code{file_name} must
-#' contain  data in teh the standard bed-format (i.e. a genomic interval
-#' represented as 0-based half-open interval with seq-id,start and end position)
-#' These columns will be renamed to 'chrom', 'start' and 'end' respectively. Any
-#' otehr columsn present in the data will be left unmodified.
+#' contain data in the standard bed format (i.e., a genomic interval
+#' represented by 0-based half-open interval with seq-id, start and end position). 
+#' These columns will be renamed to 'chrom', 'start' and 'end', respectively. Any
+#' other columns present in the data will be left unmodified.
 #' 
-#' The file is read in to memberory thorugh \code{\link{read.table}}, with the
-#' argeument  \code{sep} set to \code{'\t'} and \code{stringsAsFactors} set to
-#' FALSE. All other arguments are left as default, but arguments can be passd
+#' The file is read into memory with \code{\link{read.table}}, with the
+#' argument \code{sep} set to \code{'\t'} and \code{stringsAsFactors} set to
+#' FALSE. All other arguments are left as default, but arguments can be passed
 #' from \code{read_bed} to \code{read.table}.
 
-#' @param file_name path to the bed file to be read.
-#' @param tibble, logical. If \code{TRUE} the genomic intervals are returned as
+#' @param file_name  Path to the bed file to be read in
+#' @param tibble logical  If TRUE, the genomic intervals are returned as
 #' a tidy \code{tbl_df}.
-#' @param ... other arugments pased to \code{read.table}
+#' @param ...  Other arugments passed to \code{read.table}
 #' @return Either a \code{data.frame} or a \code{tbl_df} with at least three
-#' columns named 'chrom', 'start' and 'end'.
+#' columns named 'chrom', 'start' and 'end'
 #' @importFrom tibble as_tibble
 #' @importFrom utils read.table
 #' @examples
@@ -48,8 +48,8 @@ read_bed <- function(file_name, tibble = FALSE, ...) {
 }
 
 # paf tags encode their data type. This function returns the function
-# responsible for concerting the 'character' representation of tags value into
-# the appropirate type (used by process tags and read_paf).
+# responsible for changing the 'character' representation of tag values into
+# the appropriate type (used by process tags and read_paf).
 tag_to_type_fxn <- function(tag_tokens) {
     if (tag_tokens[[2]] %in% c("f", "H", "i")) {
         return(as.numeric)
@@ -70,19 +70,19 @@ process_tags <- function(tag_rows) {
     as.data.frame(tag_tibble) #base format is df, so force it back here
 }
 
-#' Read a genomic alignment in .paf format.
+#' Read a genomic alignment in PAF format
 #'
-#' See the package vingette for detailed information on the file format and its
+#' See the package vignette for detailed information on the file format and its
 #' representation as an R object.
 #
-#' @param file_name path to the paf file.
-#' @param tibble, logical. If \code{TRUE} the genomic alignments are returned as
+#' @param file_name  Path to the .paf file
+#' @param tibble logical  If TRUE, the genomic alignments are returned as
 #' a tidy \code{tbl_df}
-#' @return Either a \code{pafr} object (which acts as a \code{data.frame} or a
-#' \code{tbl_df} containing information on genomic alignments.
+#' @return Either a \code{pafr} object, which acts as a \code{data.frame}, or a
+#' \code{tbl_df} containing information on genomic alignments
 #' @importFrom tibble as_tibble
 #' @examples
-#' ali <- read_paf( system.file("extdata", "fungi.paf", package = "pafr") )
+#' ali <- read_paf( system.file("extdata", "fungi.paf", package="pafr") )
 #' ali
 #' @export
 read_paf <- function(file_name, tibble=FALSE) {
