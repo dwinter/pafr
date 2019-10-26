b707eb3d67229f9abbc80521470b99162ccec3f0
mpcox <murray.p.cox@gmail.com>
2019-10-26 21:48:53 +1300

    Updated documentation for scale

--- R/scale.r
+++ R/scale.r
@@ -1,8 +1,8 @@
-#' Number formatters for scales in base-pairs
+#' Number formatters for scales in base pairs
 #'
 #' For use with \code{\link{ggplot2}} 
-#' @param x The data (in base-pairs) to be fromated as as either Gb, Mb or Kb
-#' @return A character vector with with scale labels
+#' @param x  The data (in base pairs) to be formatted as Gb, Mb or Kb
+#' @return  A character vector with scale labels
 #' @examples
 #' \dontrun{
 #' ali <- read_paf(system.file("extdata", "fungi.paf", package="pafr"))
@@ -13,9 +13,9 @@
 Gb_lab <- function(x) paste(x / 1e9, "Gb")
 
 #' @export
-#' @rdname Gb_lab
+#' @rdname Mb_lab
 Mb_lab <- function(x) paste(x / 1e6, "Mb")
 
 #' @export
-#' @rdname Gb_lab
+#' @rdname Kb_lab
 Kb_lab <- function(x) paste(x / 1e3, "Kb")
